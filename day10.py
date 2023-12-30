e425ac8ec8aa13709a5019830b67a472394dd433
AlexAC <alexandreabouchahine@gmail.com>
2023-12-30 13:12:54 +0100

    fix day10 part 2

--- day10.py
+++ day10.py
@@ -1,4 +1,7 @@
 import enum
+import itertools
+import sys
+sys. setrecursionlimit(100_000)
 
 matrix = list[list[str]]
 position = tuple[int, int]
@@ -42,7 +45,6 @@ def get_path(mat: matrix, path: list[position], from_dir: Dir) -> list[position]
         return None
     pipe = mat[pos[0]][pos[1]]
     if pipe == "S":
-        print(len(path) // 2)
         return path
     pipe_dir = PIPES.get(pipe, {})
     if from_dir not in pipe_dir:
@@ -78,7 +80,7 @@ def store_output(file: str, mat: matrix):
         )
 
 
-def part1(file: str):
+def part1(file: str) -> int:
     mat = get_input(file)
     start = get_start(mat)
     for x in MAP_DIR.keys():
@@ -88,37 +90,61 @@ def part1(file: str):
             from_dir=rever_dir[x]
         )
         if p:
-            return p
-
+            return len(p) // 2
+    return -1
 
 def part2(file: str):
-    from colorama import Fore
     mat = get_input(file)
-    path = part1(file)
-    
-    vertical_signs = {"|", "L", "J", "F", "7"} 
-    
-    for i in range(len(mat)):
+    start = get_start(mat)
+    for x in MAP_DIR.keys():
+        path = get_path(
+            mat,
+            [start, (start[0]+MAP_DIR[x][0], start[1]+MAP_DIR[x][1])],
+            from_dir=rever_dir[x]
+        )
+        if path:
+            break
+
+    # Replace S with pipe
+    d_r1, d_c1 = path[1][0]-path[0][0], path[1][1]-path[0][1]
+    d_r2, d_c2 = path[-2][0]-path[0][0], path[-2][1]-path[0][1]
+    print(d_r1, d_c1)
+    print(d_r2, d_c2)
+
+    for pipe, pipe_val in PIPES.items():
+        dirs = set(MAP_DIR[d] for d in pipe_val)
+        print(dirs)
+        if {(d_r1, d_c1), (d_r2, d_c2)} == set(dirs):
+            mat[start[0]][start[1]] = pipe
+            print(pipe)
+            break
+
+    for i in range(1, len(mat)-1):
         inside = False
-        for j in range(len(mat[0])):
-            if (i, j) in path and mat[i][j] in vertical_signs:
-                inside = not inside
-            elif (i, j) not in path:
+        j = 0
+        while j < len(mat[0])-1:
+            if (i, j) not in path:
                 if inside:
                     mat[i][j] = "I"
-                else:
-                    mat[i][j] = "O"
+            else:
+                if mat[i][j] == "|":
+                    inside = not inside
+                elif mat[i][j] == "L":
+                    while j+1 < len(mat[0]) and mat[i][j+1] == "-":
+                        j += 1
+                    if mat[i][j+1] == "7":
+                        inside = not inside
+                elif mat[i][j] == "F":
+                    while j+1 < len(mat[0]) and mat[i][j+1] == "-":
+                        j += 1
+                    if mat[i][j+1] == "J":
+                        inside = not inside
+            j += 1
 
-    for i, j in path:
-        if mat[i][j] == "S":
-            mat[i][j] = Fore.GREEN + mat[i][j] + Fore.RESET
-        else:
-            mat[i][j] = Fore.RED + mat[i][j] + Fore.RESET
+    print("\n".join("".join(x) for x in mat))
 
-    store_output("day10-output.txt", mat)
+    return sum(x == "I" for x in itertools.chain.from_iterable(mat))
 
 
 if __name__ == "__main__":
-    import sys
-    sys. setrecursionlimit(100_000)
-    part2("day10-input.txt.test")
+    print(part1("day10-input.txt"))
